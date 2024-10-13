47ce7e5c30d0c6ee217df872f990e7ed005b47ca
zhangcailong <zhangcailong@joyintech.com>
2024-10-13 21:30:02 +0800

    create new cloud2024 project

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/config/RestTemplateConfig.java
@@ -0,0 +1,16 @@
+package com.atguigu.cloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced //赋予了RestTemplate负载均衡的能力
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
